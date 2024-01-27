872e0c75d8bda6935633d1692797a2cd18c79935
ghinafatima079 <155901531+ghinafatima079@users.noreply.github.com>
2024-01-27 21:43:27 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,19 @@
+#concatenation
+str1=input("Enter a string")
+str2=input("Enter annother string:")
+str3=str1+str2
+print("The new concatenated string is:"+str3)
+
+#length of string
+str1=input("Enter a string")
+print("The length of the string is",len(str1))
+
+#reverse a string
+str1=input("Enter a string")
+rev=str1[::-1]
+print("The reverse of",str1,"is",rev)
+
+#uppercase
+str1=input("Enter a string: ")
+uppercase=str1.upper()
+print("original string:",str1,"\nuppercase string:",uppercase)
